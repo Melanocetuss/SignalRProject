cf857103f08d53000be8436d310a983a7ad29b04
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-02 08:29:31 +0300

    update

---
+++ SignalR.EntityLayer/Entities/Feature.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string FirstTitle { get; set; }
+        public string FirstDescription { get; set; }
+        public string SecondTitle { get; set; }
+        public string SecondDescription { get; set; }
+        public string ThirtTitle { get; set; }
+        public string ThirtDescription { get; set; }
+    }
+}
