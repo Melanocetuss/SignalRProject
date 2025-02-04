1273bce6d81793cc23000e2f45835b96d8ad1cc7
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-04 06:19:14 +0300

    update_Layout_ComponentPartail

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutFooterComponentPartail.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutFooterComponentPartail : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
