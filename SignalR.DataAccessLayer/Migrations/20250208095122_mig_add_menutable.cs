b4c37dfcb59be8465b833bcdf4029faeb7dc5878
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-08 13:46:11 +0300

    update_MenuTableCount&Api

---
+++ SignalR.DataAccessLayer/Migrations/20250208095122_mig_add_menutable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_menutable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_moneyCases",
+                table: "moneyCases");
+
+            migrationBuilder.RenameTable(
+                name: "moneyCases",
+                newName: "MoneyCases");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MoneyCases",
+                table: "MoneyCases",
+                column: "MoneyCaseID");
+
+            migrationBuilder.CreateTable(
+                name: "MenuTables",
+                columns: table => new
+                {
+                    MenuTableID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuTables", x => x.MenuTableID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuTables");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MoneyCases",
+                table: "MoneyCases");
+
+            migrationBuilder.RenameTable(
+                name: "MoneyCases",
+                newName: "moneyCases");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_moneyCases",
+                table: "moneyCases",
+                column: "MoneyCaseID");
+        }
+    }
+}
---
+++ SignalRApi/Controllers/MenuTablesController.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MenuTableDtos;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuTablesController : ControllerBase
+    {
+        private readonly IMenuTableService _menuTableService;
+        private readonly IMapper _mapper;
+        public MenuTablesController(IMenuTableService menuTableService, IMapper mapper)
+        {
+            _menuTableService = menuTableService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenuTableList()
+        {
+            var values = _mapper.Map<List<ResultMenuTableDto>>(_menuTableService.TGetListAll());
+            return Ok(values);
+        }
+
+        [HttpGet("GetMenuTable")]
+        public IActionResult GetMenuTable(int id)
+        {
+            var value = _mapper.Map<ResultMenuTableDto>(_menuTableService.TGetByID(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult AddMenuTable(CreateMenuTableDto createMenuTableDto)
+        {
+            var menuTableEntity = _mapper.Map<MenuTable>(createMenuTableDto);
+            _menuTableService.TAdd(menuTableEntity);
+            return Ok("Masa Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            _menuTableService.TDelete(value);
+            return Ok("Masa Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+        {
+            var menuTableEntity = _mapper.Map<MenuTable>(updateMenuTableDto);
+            _menuTableService.TUpdate(menuTableEntity);
+            return Ok("Masa Güncellendi");
+        }
+
+        [HttpGet("GetMenuTableByStatusFalseCount")]
+        public IActionResult GetMenuTableByStatusFalseCount()
+        {
+            var value = _menuTableService.TGetMenuTableByStatusFalseCount();
+            return Ok(value);
+        }
+    }
+}
