7ee002d2a61e69f46a00e5312cb5fbdd687e6b37
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-13 18:45:37 +0300

    update

--- SignalRApi/Controllers/BasketsController.cs
+++ SignalRApi/Controllers/BasketsController.cs
@@ -14,10 +14,12 @@ namespace SignalRApi.Controllers
     {
         private readonly IBasketService _basketService;
         private readonly IMapper _mapper;
-        public BasketsController(IBasketService basketService, IMapper mapper)
+        private readonly SignalRContext _context;
+        public BasketsController(IBasketService basketService, IMapper mapper, SignalRContext context)
         {
             _basketService = basketService;
             _mapper = mapper;
+            _context = context;
         }
 
         [HttpGet]
@@ -30,16 +32,43 @@ namespace SignalRApi.Controllers
         [HttpPost]
         public IActionResult CreateBasket(CreateBasketDto createBasketDto)
         {
-            using var context = new SignalRContext();
-           _basketService.TAdd(new Basket
-           {
-               ProductID = createBasketDto.ProductID,
-               Count = 1,
-               MenuTableID = 2,
-               Price = context.Products.Where(x => x.ProductID == createBasketDto.ProductID).Select(x => x.Price).FirstOrDefault(),
-               TotalPrice = context.Products.Where(x => x.ProductID == createBasketDto.ProductID).Select(x => x.Price).FirstOrDefault()
-           });
-           return Ok("Sepete Eklendi");
+            // Ürünün mevcut fiyatını al
+            var productPrice = _context.Products
+                .Where(x => x.ProductID == createBasketDto.ProductID)
+                .Select(x => x.Price)
+                .FirstOrDefault();
+
+            // Sepette bu ürün var mı
+            var existingBasketItem = _context.Baskets
+                .FirstOrDefault(x => x.ProductID == createBasketDto.ProductID && x.MenuTableID == 2);
+
+            if (existingBasketItem != null)
+            {
+                // Ürün zaten sepette varsa
+                existingBasketItem.Count += 1;
+                existingBasketItem.TotalPrice = existingBasketItem.Count * existingBasketItem.Price;
+
+                _context.Baskets.Update(existingBasketItem);
+            }
+
+            // Ürün sepette yoksa
+            else
+            {
+                
+                var newBasketItem = new Basket
+                {
+                    ProductID = createBasketDto.ProductID,
+                    Count = 1,
+                    MenuTableID = 2,
+                    Price = productPrice,
+                    TotalPrice = productPrice
+                };
+
+                _context.Baskets.Add(newBasketItem);
+            }
+
+            _context.SaveChanges();
+            return Ok("Sepete Eklendi");
         }
     }
 }
