87d3f9aa5400dd5d0ec046eb1806a2fd1e615af7
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-15 18:07:00 +0300

    update_basket_bookatable

---
+++ SignalRWebUI/Controllers/BookAtableController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+    public class BookAtableController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.SubPage = "sub_page";
+            ViewBag.NavbarDiv = "</div>";
+            return View();
+        }
+    }
+}
