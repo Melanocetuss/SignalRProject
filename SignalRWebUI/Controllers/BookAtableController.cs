58ea07a3e269c0d5eee64e598399b67681fef824
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-16 16:44:43 +0300

    update_bookingbackend

--- SignalRWebUI/Controllers/BookAtableController.cs
+++ SignalRWebUI/Controllers/BookAtableController.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BookingDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
     public class BookAtableController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookAtableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.SubPage = "sub_page";
             ViewBag.NavbarDiv = "</div>";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateBookingDto createBookingDto)
+        {
+            ViewBag.SubPage = "sub_page";
+            ViewBag.NavbarDiv = "</div>";
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7274/api/Bookings", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+
             return View();
         }
     }
--- SignalRWebUI/Controllers/DefaultController.cs
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.BasketDtos;
+using SignalRWebUI.Dtos.BookingDtos;
 using System.Net.Http;
 using System.Text;
 
@@ -8,8 +9,31 @@ namespace SignalRWebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateBookingDto createBookingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7274/api/Bookings", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
     }
