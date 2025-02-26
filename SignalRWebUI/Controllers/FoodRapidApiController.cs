f0bd0321cd3d30f7aa16da85133d3b21d43313a0
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-02-26 06:29:45 +0300

    update_RapidApi

---
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    [AllowAnonymous]
+    public class FoodRapidApiController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public FoodRapidApiController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.SubPage = "sub_page";
+            ViewBag.NavbarDiv = "</div>";
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "5bb14360cdmsh17b73e85ddfcdb4p1f70edjsn46c7d722e3cc" },
+                    { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<RootTastyApiDto>(body);
+                var values = root.results;
+                return View(values);
+            }         
+        }
+    }
+}
